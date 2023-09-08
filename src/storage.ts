b542e6990acde58ab25defe6a15f89fb4da48e15
Johannes Røsvik <j.rosvik@gmail.com>
2023-09-09 00:59:57 +0200

    feat: store feature selection

---
+++ src/storage.ts
@@ -0,0 +1,18 @@
+import { LocalStorage } from "@raycast/api";
+
+enum StorageKeys {
+  preferredFeatures = "@departures/PreferredFeatures/v1",
+}
+
+export async function storePreferredVenue(featureId: string): Promise<string[]> {
+  const features = await loadPreferrededVenue();
+  const deDupedFeatures = features?.filter((f) => f !== featureId);
+  const newFeatures = deDupedFeatures ? [featureId, ...deDupedFeatures] : [featureId];
+  await LocalStorage.setItem(StorageKeys.preferredFeatures, JSON.stringify(newFeatures));
+  return newFeatures;
+}
+export async function loadPreferrededVenue(): Promise<string[] | undefined> {
+  const item = await LocalStorage.getItem<string>(StorageKeys.preferredFeatures);
+  if (!item) return undefined;
+  return JSON.parse(item) as string[];
+}
