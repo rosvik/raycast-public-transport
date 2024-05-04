082e1529c077d15e790e394a74d6169ea46cb5f9
Johannes Røsvik <j.rosvik@gmail.com>
2024-05-04 16:00:26 +0200

    chore: fix eslint issues

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import { getDepartureQuery } from "./departureQuery";
 import { Feature, QuayDeparture, QuayLineFavorites, StopPlaceQuayDeparturesQuery } from "./types";
 
-const CLIENT_NAME = "raycast-norwegian-public-transport"
+const CLIENT_NAME = "raycast-norwegian-public-transport";
 
 type FeatureResponse = {
   features: Feature[];
