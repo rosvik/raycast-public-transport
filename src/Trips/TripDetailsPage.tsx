a8cd8313e4bcc593084e8ac8b884ad9d8c57c5d3
Johannes Røsvik <j.rosvik@gmail.com>
2025-01-22 23:34:07 +0100

    feat: add destination in trip details

--- src/Trips/TripDetailsPage.tsx
+++ src/Trips/TripDetailsPage.tsx
@@ -1,5 +1,5 @@
-import { List } from "@raycast/api";
-import { formatAsDate, getTransportIcon } from "../utils";
+import { Color, Icon, List } from "@raycast/api";
+import { formatAsClock, formatAsDate, formatMillisecondsToHuman, getTransportIcon } from "../utils";
 import { getLabelText } from "./TripsPage";
 import { getTitleText } from "./TripsPage";
 import { Detail } from "../Departures/Detail";
@@ -10,8 +10,13 @@ type Props = {
 };
 export default function TripDetailsPage({ trip }: Props) {
   const date = formatAsDate(trip.expectedStartTime);
+  const fromPlace = trip.legs[0].fromPlace;
+  const toPlace = trip.legs[trip.legs.length - 1].toPlace;
   return (
-    <List isShowingDetail={true}>
+    <List
+      isShowingDetail={true}
+      searchBarPlaceholder={`From ${fromPlace.quay.name} to ${toPlace.quay.name}...`}
+    >
       <List.Section title={date === new Date().toDateString() ? "Today" : date}>
         {trip.legs.map((leg, idx) => (
           <List.Item
@@ -30,6 +35,30 @@ export default function TripDetailsPage({ trip }: Props) {
             }
           />
         ))}
+        <List.Item
+          title={`${formatAsClock(trip.expectedEndTime)} ${toPlace.quay.name} ${toPlace.quay.publicCode || ""}`}
+          accessories={[{ icon: { source: Icon.Pin, tintColor: Color.SecondaryText } }]}
+          detail={
+            <List.Item.Detail
+              metadata={
+                <List.Item.Detail.Metadata>
+                  <List.Item.Detail.Metadata.Label
+                    title="Estimated arrival"
+                    text={
+                      new Date(trip.expectedEndTime).toLocaleDateString("no-no") +
+                      " " +
+                      new Date(trip.expectedEndTime).toLocaleTimeString("no-no")
+                    }
+                  />
+                  <List.Item.Detail.Metadata.Label
+                    title="Total duration"
+                    text={formatMillisecondsToHuman(trip.duration * 1000)}
+                  />
+                </List.Item.Detail.Metadata>
+              }
+            />
+          }
+        />
       </List.Section>
     </List>
   );
